747fd4fc6a0d36a19db6f9846f290e34163c3b52
luyanci <2058282593@qq.com>
2024-05-01 16:20:29 +0800

    change

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,28 +3,29 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
 	lang: 'zh-CN',
 	base: '/',
-	title: 'Hero',
-	description: '基于 vitepress 的文档模板',
+	title: '随机点名',
+	description: '学生党的快乐利器',
 	head: [['link', { rel: 'icon', href: '/favicon.png' }]],
 	themeConfig: {
 		logo: 'logo.svg',
 		footer: {
-			message: 'Released under the MIT License.',
-			copyright: 'Copyright © 2022 markthree'
+			copyright: 'Copyright © 2021-2024 luyanci'
 		},
 		nav: [
 			{
-				text: 'Guide',
-				link: '/guide/'
+				text: 'Home',
+				link: '/'
 			},
 			{
-				text: 'About',
-				link: '/about/'
+				text: 'Guide',
+				link: '/guide/'
 			}
 		],
 		socialLinks: [
 			{
-				icon: 'github',
+				icon: {
+					svg: '<svg t="1714550882060" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2389" width="200" height="200"><path d="M512 1024C229.222 1024 0 794.778 0 512S229.222 0 512 0s512 229.222 512 512-229.222 512-512 512z m259.149-568.883h-290.74a25.293 25.293 0 0 0-25.292 25.293l-0.026 63.206c0 13.952 11.315 25.293 25.267 25.293h177.024c13.978 0 25.293 11.315 25.293 25.267v12.646a75.853 75.853 0 0 1-75.853 75.853h-240.23a25.293 25.293 0 0 1-25.267-25.293V417.203a75.853 75.853 0 0 1 75.827-75.853h353.946a25.293 25.293 0 0 0 25.267-25.292l0.077-63.207a25.293 25.293 0 0 0-25.268-25.293H417.152a189.62 189.62 0 0 0-189.62 189.645V771.15c0 13.977 11.316 25.293 25.294 25.293h372.94a170.65 170.65 0 0 0 170.65-170.65V480.384a25.293 25.293 0 0 0-25.293-25.267z" fill="#C71D23" p-id="2390"></path></svg>'
+				},
 				link: 'https://github.com/name/repo'
 			}
 		]
--- docs/index.md
+++ docs/index.md
@@ -4,28 +4,28 @@ layout: home
 sidebar: false
 
 hero:
-  name: Home
-  text: vitepress 的文档模板
-  tagline: 更快的开发开源文档
+  name: 随机点名
+  text: 
+  tagline: 学生党的快乐利器
   image:
     src: /logo.svg
     alt: Refs
   actions:
     - theme: brand
-      text: guide
+      text: 文档
       link: /guide/
     - theme: alt
-      text: about
-      link: /about/
+      text: Gitee
+      link: https://gitee.com/vc_teahouse/SJDM
 
 features:
-  - title: TODO
-    details: TODO
+  - title: 兼容性強
+    details: 无论在WinXP 还是到win11，甚至在wine 下运行，本程序都能完美兼容
     icon: ⚡
-  - title: TODO
-    details: TODO
+  - title: 无需安装配置
+    details: 以单程序运行，无论是否提供名单文件，都可使用本程序
     icon: ☁️
-  - title: TODO
-    details: TODO
+  - title: 纯中文语言开发
+    details: 本程序基于易语言开发，纯中文语言，方便维护
     icon: 🚀
 ---
